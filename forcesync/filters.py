cadeab0261e382c2de0900cd624e3315f32ea499
jorgesk8 <jorgevillegask8@gmail.com>
2026-09-10 11:48:00 -0600

    filtros_completos

--- forcesync/filters.py
+++ forcesync/filters.py
@@ -1,6 +1,14 @@
 import django_filters
+from django.db.models import Q
 
-from .models import DevDetalleCorregida
+
+from .models import (
+    DevDetalleCorregida,
+    Activity,
+    Calendar,
+    Opportunity,
+    Account,
+)
 
 
 class DevDetalleCorregidaFilter(django_filters.FilterSet):
@@ -38,4 +46,104 @@ class DevDetalleCorregidaFilter(django_filters.FilterSet):
             'fecha_hasta',
             'ingresosusd_min',
             'ingresosusd_max',
+        ]
+
+class ActivityFilter(django_filters.FilterSet):
+
+    fecha_desde = django_filters.DateFilter(
+        field_name='date',
+        lookup_expr='gte'
+    )
+
+    fecha_hasta = django_filters.DateFilter(
+        field_name='date',
+        lookup_expr='lte'
+    )
+
+    class Meta:
+        model = Activity
+
+        fields = [
+            'salesrepid_id',
+            'accountid_id',
+            'typeid_id',
+            'checkintypeid',
+            'fecha_desde',
+            'fecha_hasta',
+        ]
+
+class CalendarFilter(django_filters.FilterSet):
+
+    fecha_desde = django_filters.DateFilter(
+        field_name='enddate',
+        lookup_expr='gte'
+    )
+
+    fecha_hasta = django_filters.DateFilter(
+        field_name='enddate',
+        lookup_expr='lte'
+    )
+
+    class Meta:
+        model = Calendar
+
+        fields = [
+            'sales_rep',
+            'accountid',
+            'completed',
+            'task',
+            'typeid_id',
+            'fecha_desde',
+            'fecha_hasta',
+        ]
+
+class OpportunityFilter(django_filters.FilterSet):
+
+    fecha_desde = django_filters.DateFilter(
+        field_name='datecreated',
+        lookup_expr='gte'
+    )
+
+    fecha_hasta = django_filters.DateFilter(
+        field_name='datecreated',
+        lookup_expr='lte'
+    )
+
+    class Meta:
+        model = Opportunity
+
+        fields = [
+            'salesrepid_id',
+            'statusid_id',
+            'typeid_id',
+            'accountid1_id',
+            'fecha_desde',
+            'fecha_hasta',
+        ]
+
+class AccountFilter(django_filters.FilterSet):
+
+    vendedor = django_filters.NumberFilter(
+        method='filter_vendedor'
+    )
+
+    def filter_vendedor(self, queryset, name, value):
+        return queryset.filter(
+            Q(salesrepid1_id=value) |
+            Q(salesrepid2_id=value) |
+            Q(salesrepid3_id=value) |
+            Q(salesrepid4_id=value) |
+            Q(salesrepid5_id=value)
+        )
+
+    class Meta:
+        model = Account
+
+        fields = [
+            'segmentid_id',
+            'statusid_id',
+            'typeid_id',
+            'city',
+            'region',
+            'vendedor',
         ]
--- forcesync/views.py
+++ forcesync/views.py
@@ -1,6 +1,13 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, filters
-from .filters import DevDetalleCorregidaFilter
+
+from .filters import (
+    DevDetalleCorregidaFilter,
+    ActivityFilter,
+    CalendarFilter,
+    OpportunityFilter,
+    AccountFilter,
+)
 
 from .models import (
     ForceUser,
@@ -29,6 +36,30 @@ class ForceUserViewSet(viewsets.ReadOnlyModelViewSet):
 class ActivityViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Activity.objects.all().order_by('id')
     serializer_class = ActivitySerializer
+    filterset_class = ActivityFilter
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+
+    search_fields = [
+        'accountid_value',
+        'comment',
+        'salesrepid_value',
+        'typeid_value',
+    ]
+
+    ordering_fields = [
+        'date',
+        'checkoutdate',
+        'salesrepid_id',
+        'accountid_id',
+        'typeid_id',
+    ]
+
+    ordering = ['id']
 
 
 class CalendarViewSet(viewsets.ReadOnlyModelViewSet):
@@ -37,17 +68,91 @@ class CalendarViewSet(viewsets.ReadOnlyModelViewSet):
         .select_related('sales_rep')
         .order_by('id')
     )
+
     serializer_class = CalendarSerializer
+    filterset_class = CalendarFilter
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+
+    search_fields = [
+        'subject',
+        'comment',
+        'salesrepid_value',
+    ]
+
+    ordering_fields = [
+        'enddate',
+        'sales_rep',
+        'accountid',
+        'typeid_id',
+    ]
+
+    ordering = ['id']
 
 
 class OpportunityViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Opportunity.objects.all().order_by('id')
     serializer_class = OpportunitySerializer
+    filterset_class = OpportunityFilter
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+
+    search_fields = [
+        'reference',
+        'comments',
+        'salesrepid_value',
+        'statusid_value',
+        'typeid_value',
+    ]
+
+    ordering_fields = [
+        'datecreated',
+        'closeddate',
+        'wondate',
+        'lostdate',
+        'salesforecastdate',
+        'total',
+    ]
+
+    ordering = ['id']
 
 
 class AccountViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Account.objects.all().order_by('id')
     serializer_class = AccountSerializer
+    filterset_class = AccountFilter
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+
+    search_fields = [
+        'name',
+        'vatnumber',
+        'city',
+        'region',
+        'email',
+    ]
+
+    ordering_fields = [
+        'name',
+        'city',
+        'region',
+        'datecreated',
+        'dateupdated',
+    ]
+
+    ordering = ['id']
 
 
 class DevDetalleCorregidaViewSet(viewsets.ReadOnlyModelViewSet):
